ac76a7debbd9635cd4d1fe08d89cd1b06bb138f9
Mayank Daswani <themonk911@gmail.com>
2017-07-29 19:31:56 +1000

    Minor UI tweaks and initial content.

--- src/Controls.js
+++ src/Controls.js
@@ -32,7 +32,7 @@ class Controls extends Component {
   render() {
     return (
       <div>
-      <section className="spotlight style1 orient-right content-align-left image-position-center onscroll-image-fade-in" id="first">
+      <section className="banner style1 orient-right content-align-left image-position-center onscroll-image-fade-in" id="first">
 
         <div className="content">
           <h2>Magna etiam feugiat</h2>
