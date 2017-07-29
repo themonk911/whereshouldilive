e32e2b66e1314da631d10d812d73e738d9264bb7
Mayank Daswani <themonk911@gmail.com>
2017-07-29 12:06:55 +1000

    Working layers via heatmap.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React, { Component }  from 'react'
 import { render } from 'react-dom'
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Map, TileLayer, Marker, Popup, LayersControl, FeatureGroup} from 'react-leaflet'
 import HeatmapLayer from 'react-leaflet-heatmap-layer';
 import { addressPoints } from './realworld.10000.js';
 
 class SimpleExample extends Component {
-
-  render() {
-    return (
-      <div>
-        <Map center={[0,0]} zoom={13}>
-          <HeatmapLayer
-            fitBoundsOnLoad
-            fitBoundsOnUpdate
-            points={addressPoints}
-            longitudeExtractor={m => m[1]}
-            latitudeExtractor={m => m[0]}
-            intensityExtractor={m => parseFloat(m[2])} />
-          <TileLayer
-            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          />
-        </Map>
-      </div>
+  
+  constructor()
+  {
+    super();
+    this.position =    [50.05, -0.09];  
+  }
+ render() {
+    return (      
+      <Map center={this.position} zoom={13} >
+            <LayersControl>
+              <LayersControl.BaseLayer name="Base" checked>
+                <TileLayer
+                  url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                  attribution="&copy; <a href=http://osm.org/copyright>OpenStreetMap</a> contributors"
+                />
+              </LayersControl.BaseLayer>
+              <LayersControl.Overlay name="Heatmap" checked>
+                <FeatureGroup color="purple">
+                  <Marker position={this.position} >
+                    <Popup>
+                      <span>A pretty CSS3 popup.<br /> Easily customizable. </span>
+                    </Popup>
+                  </Marker>
+                  <HeatmapLayer
+                    fitBoundsOnLoad
+                    fitBoundsOnUpdate
+                    points={addressPoints}
+                    longitudeExtractor={m => m[1]}
+                    latitudeExtractor={m => m[0]}
+                    intensityExtractor={m => parseFloat(m[2])}
+                  />
+                </FeatureGroup>
+              </LayersControl.Overlay>
+              <LayersControl.Overlay name="Marker" checked>
+                <FeatureGroup color="purple">
+                  <Marker position={this.position} >
+                    <Popup>
+                      <span>A pretty CSS3 popup.<br /> Easily customizable. </span>
+                    </Popup>
+                  </Marker>
+                </FeatureGroup>
+              </LayersControl.Overlay>
+            </LayersControl>
+          </Map>
     );
   }
 
