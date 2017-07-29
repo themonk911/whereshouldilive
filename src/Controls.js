ea22e8d263949dcbc32448ba28da27a23dd162fe
Kenli Chong <klchong@gmail.com>
2017-07-29 19:18:16 +1000

    Map and sliders at right spot.

--- src/Controls.js
+++ src/Controls.js
@@ -32,21 +32,25 @@ class Controls extends Component {
   render() {
     return (
       <div>
-        <div class="content">
+      <section className="spotlight style1 orient-right content-align-left image-position-center onscroll-image-fade-in" id="first">
+
+        <div className="content">
           <h2>Magna etiam feugiat</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum dolor sit amet.</p>
-          <ul class="actions vertical">
-            <li><a href="#" class="button">Learn More</a></li>
-            <Slider step={10} dots value={this.state.intensity} onChange={this.handleChange}/>
 
+          <ul className="actions vertical">
+            <li><a href="#" className="button">Learn More</a></li>
+            <Slider step={10} dots value={this.state.intensity} onChange={this.handleChange} />
           </ul>
 
           <div id='counter'>{this.state.intensity}</div>
         </div>
 
-        <div class="image">
+        <div className="image">
           <SimpleExample intensity={this.state.intensity} />
         </div>
+        </section>
+
 </div>
 
         // <SimpleExample intensity={this.state.intensity} />
