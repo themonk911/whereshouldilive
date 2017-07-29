3bb533cda058028f013e600d1c0b31e5583ec27a
Mayank Daswani <themonk911@gmail.com>
2017-07-29 11:37:53 +1000

    Working heatmap without layers.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 import React, { Component }  from 'react'
 import { render } from 'react-dom'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import HeatmapLayer from 'react-leaflet-heatmap-layer';
+import { addressPoints } from './realworld.10000.js';
 
 class SimpleExample extends Component {
-  constructor() {
-    super()
-    this.state = {
-      lat: -35.325,
-      lng: 149.13,
-      zoom: 13
-    }
-  }
 
   render() {
-    const position = [this.state.lat, this.state.lng];
     return (
-      <Map center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-        />
-        <Marker position={position}>
-          <Popup>
-            <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
-          </Popup>
-        </Marker>
-      </Map>
+      <div>
+        <Map center={[0,0]} zoom={13}>
+          <HeatmapLayer
+            fitBoundsOnLoad
+            fitBoundsOnUpdate
+            points={addressPoints}
+            longitudeExtractor={m => m[1]}
+            latitudeExtractor={m => m[0]}
+            intensityExtractor={m => parseFloat(m[2])} />
+          <TileLayer
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </Map>
+      </div>
     );
   }
+
 }
 
 export default SimpleExample;
