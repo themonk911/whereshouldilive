2f76ce2ce4c9899a3e3cc4a13ab7a53025c4782c
Kenli Chong <klchong@gmail.com>
2017-07-29 20:49:14 +1000

    Setup transitions.

--- src/Controls.js
+++ src/Controls.js
@@ -32,7 +32,7 @@ class Controls extends Component {
   render() {
     return (
       <div>
-      <section className="banner style1 orient-right content-align-left image-position-center onscroll-image-fade-in" id="first">
+      <section className="banner style1 orient-right content-align-left image-position-center onscroll-image-fade-in" id="second">
 
         <div className="content">
           <h2>Magna etiam feugiat</h2>
@@ -60,7 +60,7 @@ class Controls extends Component {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum dolor sit amet.</p>
 
           <ul className="actions vertical">
-            <li><a href="#" className="button">Learn More</a></li>
+            <li><a href="#third" className="button">Learn More</a></li>
           </ul>
 
 
